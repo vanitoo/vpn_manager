cb2e16f4fbda915287a7ee5a53cb67fff5369d41
vanitoo <56897888+vanitoo@users.noreply.github.com>
2026-09-08 22:09:29 +0300

    Bump Warp version to 0.8.4

--- app/version.py
+++ app/version.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 APP_NAME = 'Warp Private Network'
-APP_VERSION = '0.8.3'
-BUILD_DATE = '2026-09-06'
+APP_VERSION = '0.8.4'
+BUILD_DATE = '2026-09-08'
 
 # Version of the SQLite database structure expected by this application.
 DB_SCHEMA_VERSION = 1
